4e9f19a28bfa6c71cfc31d9fdde36ee1aa069675
tndk <ys0v9@gmail.com>
2025-07-12 15:49:04 +0900

    create UserService

--- src/main/java/com/example/practice1/service/UserService.java
+++ src/main/java/com/example/practice1/service/UserService.java
@@ -1,9 +1,21 @@
 package com.example.practice1.service;
 
+import com.example.practice1.domain.User;
 import com.example.practice1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
     private UserRepository userRepository;
+
+    public void register(User user) {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            throw new RuntimeException("This username is already taken");
+        }
+
+        userRepository.save(user);
+    }
 }
