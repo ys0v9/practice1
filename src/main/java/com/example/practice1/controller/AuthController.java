77ac528f330e2e0f6c7f035601eb90a7511633e7
tndk <sua9097@gmail.com>
2025-07-12 15:57:52 +0900

    create RegisterRequest

--- src/main/java/com/example/practice1/controller/AuthController.java
+++ src/main/java/com/example/practice1/controller/AuthController.java
@@ -1,4 +1,23 @@
 package com.example.practice1.controller;
 
+
+import com.example.practice1.dto.RegisterRequest;
+import com.example.practice1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody RegisterRequest registerRequest) {
+    }
+
 }
--- src/main/java/com/example/practice1/dto/RegisterRequest.java
+++ src/main/java/com/example/practice1/dto/RegisterRequest.java
@@ -1,4 +1,10 @@
 package com.example.practice1.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class RegisterRequest {
+    private String username;
+    private String password;
 }
