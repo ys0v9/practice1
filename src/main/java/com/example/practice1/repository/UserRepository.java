37b3b7d2dc803d6a772cb6cc59d05c3d26215e60
tndk <tndk@tndkui-MacBookPro.local>
2025-07-12 15:38:14 +0900

    add UserRepository Interface

--- src/main/java/com/example/practice1/repository/UserRepository.java
+++ src/main/java/com/example/practice1/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.practice1.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String username);
