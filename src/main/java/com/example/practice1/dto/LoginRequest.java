21ba5f147698a2a82c46061d361e07b361b7f1fd
tndk <sua9097@gmail.com>
2025-07-12 15:59:58 +0900

    feat: create LoginRequest

--- src/main/java/com/example/practice1/dto/LoginRequest.java
+++ src/main/java/com/example/practice1/dto/LoginRequest.java
@@ -1,4 +1,11 @@
 package com.example.practice1.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    private String username;
+    private String password;
 }
