a1552f1e94853a75d7a2d0cff47582c67ed6cfac
tndk <sua9097@gmail.com>
2025-07-14 08:54:00 +0900

    create PostRequest

---
+++ src/main/java/com/example/practice1/dto/PostRequest.java
@@ -0,0 +1,11 @@
+package com.example.practice1.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostRequest {
+    private String title;
+    private String content;
+}
