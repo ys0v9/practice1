92b378ec7ee34f7ae3adb01f66a49f16090c4c6e
tndk <sua9097@gmail.com>
2025-07-13 20:13:32 +0900

    feat: implement Post entity

--- src/main/java/com/example/practice1/domain/Post.java
+++ src/main/java/com/example/practice1/domain/Post.java
@@ -1,4 +1,25 @@
 package com.example.practice1.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@Table(name = "Board")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
 }
